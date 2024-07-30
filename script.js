d61b3d51c91e9eb63bb5eae0a7d8a31343eb7be2
Tiago Macedo Cardoso <contato.cardosotiago@gmail.com>
2024-07-30 04:30:30 -0300

    Adição de uma nova funcionalidade afim de melhorar a usabilidade final do projeto, evitando erros do usuário e duplicidades em nomes de logradouros que se repetem em bairros diferentes. Esse commit pussui também algumas mudanças no visual da aplicação

--- script.js
+++ script.js
@@ -1,54 +1,3 @@
-/* document.addEventListener('DOMContentLoaded', function() {
-    const filePath = 'public/Publicações 15 07 2024.csv';
-
-    fetch(filePath)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erro ao carregar o arquivo CSV');
-            }
-            return response.text();
-        })
-        .then(csvText => {
-            Papa.parse(csvText, {
-                header: true,
-                dynamicTyping: true,
-                complete: function(results) {
-                    const data = results.data;
-
-                    const datalist = document.getElementById('logradouros');
-
-                    data.forEach(endereco => {
-                        if (endereco.Logradouro) {
-                            const option = document.createElement('option');
-                            option.value = endereco.Logradouro;
-                            datalist.appendChild(option);
-                        }
-                    });
-
-                    document.getElementById('buscar').addEventListener('click', function() {
-                        const logradouroPesquisado = document.getElementById('logradouro').value;
-                        const enderecoEncontrado = data.find(endereco => endereco.Logradouro && endereco.Logradouro.toLowerCase() === logradouroPesquisado.toLowerCase());
-
-                        const resultado = document.getElementById('resultado');
-                        const cep_resultado = document.getElementById('cep_resultado');
-                        if (enderecoEncontrado) {
-                            resultado.textContent = `O CEP para o logradouro "${logradouroPesquisado}" é:`;
-                            cep_resultado.textContent = ` ${enderecoEncontrado.CEP}`;
-                        } else {
-                            resultado.textContent = 'Logradouro não encontrado';
-                        }
-                    });
-                },
-                error: function(error) {
-                    console.error('Error parsing CSV:', error);
-                }
-            });
-        })
-        .catch(error => {
-            console.error('Erro ao carregar o arquivo:', error);
-        });
-}); */
-
 document.addEventListener('DOMContentLoaded', function() {
     const filePath = 'public/Publicações 15 07 2024.csv';
 
@@ -65,19 +14,48 @@ document.addEventListener('DOMContentLoaded', function() {
                 dynamicTyping: true,
                 complete: function(results) {
                     const data = results.data;
-                    const select = document.getElementById('logradouro');
+                    const bairroSelect = document.getElementById('bairro');
+                    const logradouroSelect = document.getElementById('logradouro');
 
-                    data.forEach(endereco => {
-                        if (endereco.Logradouro) {
-                            const option = document.createElement('option');
-                            option.value = endereco.Logradouro;
-                            option.textContent = endereco.Logradouro;
-                            select.appendChild(option);
+                    // Obtenha a lista única de bairros
+                    const bairros = [...new Set(data.map(endereco => endereco.Bairro).filter(Boolean))];
+
+                    // Preencha o select de bairros
+                    bairros.forEach(bairro => {
+                        const option = document.createElement('option');
+                        option.value = bairro;
+                        option.textContent = bairro;
+                        bairroSelect.appendChild(option);
+                    });
+
+                    // Evento para filtrar logradouros com base no bairro selecionado
+                    bairroSelect.addEventListener('change', function() {
+                        const bairroSelecionado = bairroSelect.value;
+                        // Limpe o select de logradouros
+
+                        if (bairroSelecionado == '') {
+                            logradouroSelect.innerHTML = '<option value="">Selecione primeiramente um bairro</option>';
+                        } else {
+                            logradouroSelect.innerHTML = '<option value="">Selecione um logradouro</option>';
                         }
+                        
+
+                        // Filtre logradouros com base no bairro selecionado
+                        const logradourosFiltrados = data.filter(endereco => endereco.Bairro === bairroSelecionado);
+
+                        // Preencha o select de logradouros
+                        logradourosFiltrados.forEach(endereco => {
+                            if (endereco.Logradouro) {
+                                const option = document.createElement('option');
+                                option.value = endereco.Logradouro;
+                                option.textContent = endereco.Logradouro;
+                                logradouroSelect.appendChild(option);
+                            }
+                        });
                     });
 
                     document.getElementById('buscar').addEventListener('click', function() {
-                        const logradouroPesquisado = select.value;
+                        const logradouroPesquisado = logradouroSelect.value;
                         const enderecoEncontrado = data.find(endereco => endereco.Logradouro === logradouroPesquisado);
 
                         const resultado = document.getElementById('resultado');
